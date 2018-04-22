a90156b2689f1b2c4f65a6b19b5fa54bb7f29a12
LosBooom <visitors.there@gmail.com>
2018-04-22 19:02:27 +0300

    added 2 buttons

--- lab7/lab7_gui.py
+++ lab7/lab7_gui.py
@@ -10,6 +10,11 @@ my_notes = [{'id': 0, 'name': 'name_0', 'unp': '024123', 'fio': 'Vasya, kek', 'w
             {'id': 3, 'name': 'name_3', 'unp': '3123', 'fio': 'Jora, kek', 'work': 'Закрыто', 'date': '11.21.1991'},
             {'id': 4, 'name': 'name_4', 'unp': '6534634', 'fio': 'Kisa, kek', 'work': 'Активно', 'date': '01.01.2001'},
             {'id': 6, 'name': 'name_6', 'unp': '3213123', 'fio': 'doog, kek', 'work': 'Закрыто', 'date': '01.31.2005'},
+            {'id': 7, 'name': 'name_0', 'unp': '024123', 'fio': 'Vasya, kek', 'work': 'Активно', 'date': '01.01.1998'},
+            {'id': 9, 'name': 'name_1', 'unp': '4234123', 'fio': 'Petya, kek', 'work': 'Активно', 'date': '03.17.1998'},
+            {'id': 10, 'name': 'name_3', 'unp': '3123', 'fio': 'Jora, kek', 'work': 'Закрыто', 'date': '11.21.1991'},
+            {'id': 11, 'name': 'name_4', 'unp': '6534634', 'fio': 'Kisa, kek', 'work': 'Активно', 'date': '01.01.2001'},
+            {'id': 13, 'name': 'name_6', 'unp': '3213123', 'fio': 'doog, kek', 'work': 'Закрыто', 'date': '01.31.2005'},
             ]
 
 # main loop
@@ -20,7 +25,7 @@ root.title('Lab7-top-10/10-magic')
 
 # head
 def head_line_draw(row):
-    '''Принимает номер строки'''
+    """Принимает номер строки"""
     label_id = Label(root, text='ID', font=('Mono', 15))
     label_name = Label(root, text='Название', font=('Mono', 15))
     label_unp = Label(root, text='УНП', font=('Mono', 15))
@@ -65,11 +70,56 @@ def body_line_draw(row, data):
 
 
 def save_changes():
+    global my_notes
+
     pass
 
 
-def about_button():
-    window = root2.Tk()
+def draw_buttons(buttons):
+    i = 1
+    for button in buttons:
+        button.grid(row=i, column=6)
+        i += 1
+
+
+def erase_all():
+    list = root.grid_slaves()
+    for l in list:
+        l.grid_forget()
+
+
+def hide_closed():
+    global my_notes, buttons
+    # Очистить все поля
+    erase_all()
+
+    # перерисовать заново
+    # Отрисовка шапки на линии 0
+    head_line_draw(0)
+
+    # Отрисовка основных полей
+    i = 0
+    for my_note in my_notes:
+        i += 1
+        if my_note['work'] == 'Закрыто':
+            i -= 1
+            continue
+        body_line_draw(i, my_note.values())
+    draw_buttons(buttons)
+
+
+def show_all():
+    global my_notes
+    # перерисовать заново
+    # Отрисовка шапки на линии 0
+    head_line_draw(0)
+
+    # Отрисовка основных полей
+    i = 0
+    for my_note in my_notes:
+        i += 1
+        body_line_draw(i, my_note.values())
+    draw_buttons(buttons)
 
 
 # Отрисовка шапки на линии 0
@@ -82,58 +132,14 @@ for my_note in my_notes:
     i += 1
 
 save_button = Button(root, text='Сохранить изменения', command=save_changes)
-save_button.grid(row=1, column=6)
 
 about_button = Button(root, text='Помощь', command=lambda: showinfo('Помощь', 'Программа'))
-about_button.grid(row=2, column=6)
-# # buttons
-# btn_solve = Button(root, text='Solve', font=(FONT_TEXT, 15), width=10, command=solve)
-# btn_randomize = Button(root, text='Randomize', font=(FONT_TEXT, 15), width=10, command=randomize)
-# btn_set = Button(root, text='Set to zero', font=(FONT_TEXT, 15), width=10, command=set_to_zero)
-# btn_info = Button(root, text='About', font=(FONT_TEXT, 15), width=8, command=lambda: showinfo('Guide', about_text))
-#
-# # grid
-# label_array_a.grid(row=0, columnspan=9)
-# label_array_b.grid(row=2, columnspan=9)
-# label_array_c.grid(row=4, columnspan=9)
-#
-# # setting lists of entries and labels in window
-# index = 0
-# for entry in list_entry_a:
-#     entry.grid(row=1, column=index)
-#     index += 1
-#
-# index = 0
-# for entry in list_entry_b:
-#     entry.grid(row=3, column=index)
-#     index += 1
-#
-# index = 0
-# for label in list_label_c:
-#     label.grid(row=4, column=index)
-#     index += 1
-#
-# # buttons in window
-# btn_randomize.grid(row=1, column=10)
-# btn_solve.grid(row=0, column=10)
-# btn_info.grid(row=5, column=10)
-#
-# # ...
-# label_answer1.grid(row=2, rowspan=2, column=10)
-# label_answer2.grid(row=4, column=10)
-#
-# # image
-# photo = PhotoImage(file='1.gif')
-# w = Label(root, image=photo)
-# w.photo = photo
-# w.grid(row=6, columnspan=11)
+
+hide_button = Button(root, text='Скрыть закрытые', command=hide_closed)
+
+show_button = Button(root, text='Показать все', command=show_all)
+
+buttons = [save_button, hide_button, show_button, about_button]
+draw_buttons(buttons)
 
 root.mainloop()
-
-
-def main():
-    my_note = {'id': 0, 'name': 'name_0', 'unp': '024123', 'fio': 'Vasya, kek', 'work': 'Cookies', 'date': '01.01.1998'}
-
-
-if __name__ == "__main__":
-    main()
