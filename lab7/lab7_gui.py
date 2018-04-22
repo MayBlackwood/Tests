b843e0549812dfd4d3747af166076c5a274fb681
LosBooom <visitors.there@gmail.com>
2018-04-22 16:52:08 +0300

    added 2 buttons and reformat body lines

--- lab7/lab7_gui.py
+++ lab7/lab7_gui.py
@@ -5,6 +5,13 @@ from tkinter import *
 from tkinter.messagebox import *
 import random
 
+my_notes = [{'id': 0, 'name': 'name_0', 'unp': '024123', 'fio': 'Vasya, kek', 'work': 'Активно', 'date': '01.01.1998'},
+            {'id': 1, 'name': 'name_1', 'unp': '4234123', 'fio': 'Petya, kek', 'work': 'Активно', 'date': '03.17.1998'},
+            {'id': 3, 'name': 'name_3', 'unp': '3123', 'fio': 'Jora, kek', 'work': 'Закрыто', 'date': '11.21.1991'},
+            {'id': 4, 'name': 'name_4', 'unp': '6534634', 'fio': 'Kisa, kek', 'work': 'Активно', 'date': '01.01.2001'},
+            {'id': 6, 'name': 'name_6', 'unp': '3213123', 'fio': 'doog, kek', 'work': 'Закрыто', 'date': '01.31.2005'},
+            ]
+
 # main loop
 root = Tk()
 # window title
@@ -18,7 +25,7 @@ def head_line_draw(row):
     label_name = Label(root, text='Название', font=('Mono', 15))
     label_unp = Label(root, text='УНП', font=('Mono', 15))
     label_fio = Label(root, text='ФИО', font=('Mono', 15))
-    label_work = Label(root, text='Вид деятельности', font=('Mono', 15))
+    label_work = Label(root, text='Статус', font=('Mono', 15))
     label_date = Label(root, text='Дата регистрации', font=('Mono', 15))
 
     head_labels = [label_id, label_name, label_unp, label_fio, label_work, label_date]
@@ -31,19 +38,23 @@ def head_line_draw(row):
 
 
 def body_line_draw(row, data):
-    '''Принимает номер строки и лист из 6 полей'''
+    """Принимает номер строки и лист из 6 полей"""
     body_id = Entry(root, font=('Mono', 15), width=4)
     body_name = Entry(root, font=('Mono', 15))
     body_unp = Entry(root, font=('Mono', 15), width=10)
     body_fio = Entry(root, font=('Mono', 15))
-    body_work = Entry(root, font=('Mono', 15))
+    body_work = Entry(root, font=('Mono', 15), width=7)
     body_date = Entry(root, font=('Mono', 15), width=10)
 
     body_entries = [body_id, body_name, body_unp, body_fio, body_work, body_date]
 
     i = 0
+    my_data = []
+    for val in data:
+        my_data.append(val)
+
     for entry in body_entries:
-        entry.insert(0, data[i])
+        entry.insert(0, my_data[i])
         i += 1
 
     i = 0
@@ -53,12 +64,28 @@ def body_line_draw(row, data):
     return None
 
 
+def save_changes():
+    pass
+
+
+def about_button():
+    window = root2.Tk()
+
+
 # Отрисовка шапки на линии 0
 head_line_draw(0)
-body_line_draw(1, ['gay', 'ser', 'gay', 'ser', 'gay', 'ser'])
-body_line_draw(2, ['gay', 'ser', 'gay', 'ser', 'gay', 'ser'])
-body_line_draw(3, ['gay', 'ser', 'gay', 'ser', 'gay', 'ser'])
 
+# Отрисовка основных полей
+i = 1
+for my_note in my_notes:
+    body_line_draw(i, my_note.values())
+    i += 1
+
+save_button = Button(root, text='Сохранить изменения', command=save_changes)
+save_button.grid(row=1, column=6)
+
+about_button = Button(root, text='Помощь', command=lambda: showinfo('Помощь', 'Программа'))
+about_button.grid(row=2, column=6)
 # # buttons
 # btn_solve = Button(root, text='Solve', font=(FONT_TEXT, 15), width=10, command=solve)
 # btn_randomize = Button(root, text='Randomize', font=(FONT_TEXT, 15), width=10, command=randomize)
@@ -105,13 +132,6 @@ root.mainloop()
 
 
 def main():
-    notes = [{'id': 0, 'name': 'name_0', 'unp': '024123', 'fio': 'Vasya, kek', 'work': 'Cookies', 'date': '01.01.1998'},
-             {'id': 1, 'name': 'name_1', 'unp': '4234123', 'fio': 'Petya, kek', 'work': 'Zan', 'date': '03.17.1998'},
-             {'id': 3, 'name': 'name_3', 'unp': '3123', 'fio': 'Jora, kek', 'work': 'none', 'date': '11.21.1991'},
-             {'id': 4, 'name': 'name_4', 'unp': '6534634', 'fio': 'Kisa, kek', 'work': 'any', 'date': '01.01.2001'},
-             {'id': 6, 'name': 'name_6', 'unp': '3213123', 'fio': 'doog, kek', 'work': 'lame', 'date': '01.31.2005'},
-             ]
-
     my_note = {'id': 0, 'name': 'name_0', 'unp': '024123', 'fio': 'Vasya, kek', 'work': 'Cookies', 'date': '01.01.1998'}
 
 
